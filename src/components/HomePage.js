0089f2d4836d14acac8ee443a72151ba5f6a0964
Jay <nhanlnt@hotmail.com>
2021-03-26 11:05:52 +0700

    finished project

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,23 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import moment from 'moment';
 
+import '../styles/HomePage.css'
+
 const HomePage = () => {
-  const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
-  useEffect(() => {
-    const timer = setInterval(() => setDate(new Date()), 1000)
-    return function cleanup() {
-      clearInterval(timer)
-    }
+    useEffect(() => {
+        const timer = setInterval(() => setDate(new Date()), 1000)
+        return function cleanup() {
+            clearInterval(timer)
+        }
 
-  });
+    });
 
-  return (
-    <div>
-      <p> Time : {moment(date.toLocaleTimeString()).format('HH:mm:ss')}</p>
-      <p> Date : {date.toLocaleDateString()}</p>
-    </div>
-  );
+    return (
+        <div className="homepage-component">
+            {/*<p> Time : {moment(date.getTime()).format('HH:mm:ss')}</p>*/}
+            {/*<p> Date : {moment(date).format('DD/MM/YYYY')}</p>*/}
+            <div className="homepage__time">
+                <div className="border__style">
+                    <p>{moment(date.getTime()).format('HH')}</p>
+                </div>
+                <div className="space__style">
+                    <p>:</p>
+                </div>
+                <div className="border__style">
+                    <p>{moment(date.getTime()).format('mm')}</p>
+                </div>
+                <div className="space__style">
+                    <p>:</p>
+                </div>
+                <div className="border__style">
+                    <p>{moment(date.getTime()).format('ss')}</p>
+                </div>
+            </div>
+
+            <div className="homepage__date">
+                <div className="border__style">
+                    <p>{date.getDate()}</p>
+                </div>
+                <div className="space__style">
+                    <p>/</p>
+                </div>
+                <div className="border__style">
+                    <p>{(date.getMonth() + 1)}</p>
+                </div>
+                <div className="space__style">
+                    <p>/</p>
+                </div>
+                <div className="border__style border__style__year">
+                    <p>{date.getFullYear()}</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default HomePage;
--- src/container/Country/CountryDetail.js
+++ src/container/Country/CountryDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import {useSelector} from 'react-redux';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import TableNameCountry from './TableNameCountry';
@@ -11,50 +11,50 @@ import TableCodesCountry from './TableCodesCountry';
 import HomePage from '../../components/HomePage';
 
 const CountryDetail = () => {
-  const state = useSelector(state => ({...state.countriesData}));
+    const state = useSelector(state => ({...state.countriesData}));
 
-  return (
-    <div className="country-component">
-      {
-        state.countryDetail ? (
-          <div className="country__detail__container">
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Paper className="country__detail__paper">
-                  <h1 style={{marginBottom: 0}}>{state.countryDetail.name}</h1>
-                </Paper>
-              </Grid>
-              <Grid item xs={6}>
-                <Paper className="country__detail__paper">
-                  <TableNameCountry/>
-                </Paper>
-              </Grid>
-              <Grid item xs={6}>
-                <Paper className="country__detail__paper">
-                  <img src={state.countryDetail.flag} alt={`${state.countryDetail.name}-flag`}/>
-                </Paper>
-              </Grid>
-              <Grid item xs={4}>
-                <Paper className="country__detail__paper">
-                  <TableGeographyCountry/>
-                </Paper>
-              </Grid>
-              <Grid item xs={4}>
-                <Paper className="country__detail__paper">
-                  <TableLanguageCountry/>
-                </Paper>
-              </Grid>
-              <Grid item xs={4}>
-                <Paper className="country__detail__paper">
-                  <TableCodesCountry/>
-                </Paper>
-              </Grid>
-            </Grid>
-          </div>
-        ) : <HomePage/>
-      }
-    </div>
-  );
+    return (
+        <div className="country-component">
+            {
+                state.countryDetail ? (
+                    <div className="country__detail__container">
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <Paper className="country__detail__paper">
+                                    <h1 style={{marginBottom: 0}}>{state.countryDetail.name}</h1>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={7}>
+                                <Paper className="country__detail__paper">
+                                    <TableNameCountry/>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={5}>
+                                <Paper className="country__detail__paper">
+                                    <img src={state.countryDetail.flag} alt={`${state.countryDetail.name}-flag`}/>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Paper className="country__detail__paper">
+                                    <TableGeographyCountry/>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Paper className="country__detail__paper">
+                                    <TableLanguageCountry/>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Paper className="country__detail__paper">
+                                    <TableCodesCountry/>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </div>
+                ) : <HomePage/>
+            }
+        </div>
+    );
 }
 
 export default CountryDetail;
