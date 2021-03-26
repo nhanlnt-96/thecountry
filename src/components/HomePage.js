56a5cfc1acdd29e8c56b8f428a171dca14ad9e4e
Lnt.Nhan <nhanlnt@hotmail.com>
2021-03-26 22:19:07 +0700

    complete project

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import {Empty} from "antd";
 
 import '../styles/HomePage.css'
 
 const HomePage = () => {
 
-    return (
-        <div className="homepage-component">
-            <h1>Hello, I am Jay</h1>
-            <h2>This site is intended as a easy way to explore infomation about the countries and regions of the
-                world.</h2>
-            <h3>Site design is copyright (c) 2021 Jay.</h3>
-        </div>
-    );
+  return (
+    <div className="homepage-component">
+      <p>No country has been selected</p>
+    </div>
+  );
 }
 
 export default HomePage;
